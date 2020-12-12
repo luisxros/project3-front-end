e7978955bbe7cf49dc464d3a9f1d496e6b6335b5
Luis Rosario <luisalmonter@gmail.com>
2020-12-11 20:42:57 -0500

    adds login functionality and pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
+import DashboardPage from './pages/DashboardPage';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+
+import { Route, Switch, withRouter, Redirect } from "react-router-dom";
+
+import { useState, useEffect } from 'react';
+
+import { getUser,logout } from './services/userService';
+
+
+
+function App(props) {
+
+  /* Component state*/
+
+  const [ userState, setUserState ] = useState({ user: getUser() });
+ 
+  
+
+  
+
+  /* Helper function */
+
+  function handleSignupOrLogin() {
+    setUserState({ user: getUser() });
+    props.history.push('/dashboard')
+  }
+
+  function handleLogout() {
+    logout();
+    setUserState({ user: null });
+    props.history.push('/homepage')
+  }
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header user={userState.user} handleLogout={handleLogout} />
+
+      <Switch>
+      <Route exact path="/"  render={ props =>
+        <HomePage />
+      }/>
+      <Route exact path="/dashboard"  render={ props =>
+      getUser() ? 
+        <DashboardPage />
+        :
+        <Redirect to="/login" />
+      }/>
+      <Route exact path="/login"  render={ props =>
+        <LoginPage  handleSignupOrLogin={handleSignupOrLogin} />
+      }/>
+      <Route exact path="/signup"  render={ props =>
+        <SignupPage handleSignupOrLogin={handleSignupOrLogin} />
+      }/>
+      </Switch>
+
+      <Footer />
     </div>
   );
 }
 
-export default App;
+export default withRouter(App);
