c1a4ef64c3d57f4069ac5a61e9a202edc11af0e6
Luis Rosario <luisalmonter@gmail.com>
2020-12-11 20:46:38 -0500

    adds packages

---
+++ src/components/Header/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Header.css';
+import { Link } from 'react-router-dom'
+
+function Header(props) {
+    return(
+        
+        <header className="Header">
+            <Link to="/"><h1>Find It</h1></Link>
+            <nav>
+                <ul className="NavLinks">
+                    {
+                        props.user ?
+                    <>
+                    <li> <Link to="/dashboard">Dashboard</Link></li>
+                     <span className='NavBar-welcome'>Welcome, {props.user.name}</span>
+                    <li> <Link to="" onClick={props.handleLogout}>Logout</Link></li>
+                    </>
+                    :
+                    <>
+                    <li> <Link to="/signup">Signup</Link></li>
+                    <li> <Link to="/login">Login</Link></li>
+                    </>
+                    }
+
+                </ul>
+            </nav>
+        </header>
+        
+    );
+}
+
+export default Header;
