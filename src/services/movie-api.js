9a9fb7c717d4dd6416d151d870022b11b0b36ec7
Luis Rosario <luisalmonter@gmail.com>
2020-12-13 17:21:54 -0500

    name changes

---
+++ src/services/movie-api.js
@@ -0,0 +1,5 @@
+const BASE_URL = 'https://api.themoviedb.org/3/trending';
+
+export function getMovieDay() {
+    return fetch(BASE_URL + '/movie/day?api_key=1066af0eb8a5037cd726e43f44eb6528').then(res => res.json());
+}
