e5373242c9a862d912a9a4b2050de2fae502f2c8
Luis Rosario <luisalmonter@gmail.com>
2020-12-13 18:21:52 -0500

    adds tv show api

--- src/services/movie-api.js
+++ src/services/movie-api.js
@@ -3,3 +3,7 @@ const BASE_URL = 'https://api.themoviedb.org/3/trending';
 export function getMovieDay() {
     return fetch(BASE_URL + '/movie/day?api_key=1066af0eb8a5037cd726e43f44eb6528').then(res => res.json());
 }
+
+export function getTvDay() {
+    return fetch(BASE_URL + '/tv/day?api_key=1066af0eb8a5037cd726e43f44eb6528').then(res => res.json());
+}
