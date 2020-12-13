2acf3788efd5c5aa224739c9b875325ceaedd7b7
Luis Rosario <luisalmonter@gmail.com>
2020-12-13 17:21:34 -0500

    changes name

---
+++ src/pages/MoviePage.js
@@ -0,0 +1,18 @@
+
+
+function MoviePage(props) {
+
+    
+    return (
+        <main className="Page">
+          
+        
+                <h1>{props.result.title}</h1>
+            
+          
+        </main>
+        
+    )
+}
+
+export default MoviePage;
