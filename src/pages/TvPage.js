84d418a220a23bca3815a8251ef667628f5f20c2
Luis Rosario <luisalmonter@gmail.com>
2020-12-13 18:21:09 -0500

    shows tv shows trending

---
+++ src/pages/TvPage.js
@@ -0,0 +1,18 @@
+
+
+function TvPage(props) {
+
+    
+    return (
+        <main className="Page">
+          
+        
+                <h1>{props.result.name}</h1>
+            
+          
+        </main>
+        
+    )
+}
+
+export default TvPage;
