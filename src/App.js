e7674a1fbbb2be413f3310985c2e0f08b0d3d68a
Luis Rosario <luisalmonter@gmail.com>
2020-12-13 17:24:12 -0500

    shows data of movies

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 
-import DashboardPage from './pages/DashboardPage';
+import MoviePage from './pages/MoviePage';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
@@ -13,6 +13,7 @@ import { Route, Switch, withRouter, Redirect } from "react-router-dom";
 import { useState, useEffect } from 'react';
 
 import { getUser,logout } from './services/userService';
+import { getMovieDay } from './services/movie-api';
 
 
 
@@ -21,16 +22,22 @@ function App(props) {
   /* Component state*/
 
   const [ userState, setUserState ] = useState({ user: getUser() });
+
+  const [ movieData, setMovieData ] = useState({
+    page: 0,
+    results: [],
+    total_pages: 0,
+    total_results: 0,
+  })
  
   
 
   
-
   /* Helper function */
 
   function handleSignupOrLogin() {
     setUserState({ user: getUser() });
-    props.history.push('/dashboard')
+    props.history.push('/movie')
   }
 
   function handleLogout() {
@@ -39,7 +46,19 @@ function App(props) {
     props.history.push('/homepage')
   }
 
-  
+
+  async function getMovieData() {
+    const data = await getMovieDay();
+    setMovieData(data);
+  }
+
+
+
+  useEffect(() => {
+    getMovieData();
+  }, []);
+
+
 
   return (
     <div className="App">
@@ -49,9 +68,14 @@ function App(props) {
       <Route exact path="/"  render={ props =>
         <HomePage />
       }/>
-      <Route exact path="/dashboard"  render={ props =>
+      <Route exact path="/movie"  render={ props =>
+
       getUser() ? 
-        <DashboardPage />
+      <div>
+        {movieData.results.map((result, idx) => (
+           <MoviePage result={result}/>
+        ))}
+      </div>
         :
         <Redirect to="/login" />
       }/>
