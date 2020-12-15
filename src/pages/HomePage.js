9d0c80b2d07216ba9e1a40633ca2315ca640a40b
Luis Rosario <luisalmonter@gmail.com>
2020-12-14 23:26:30 -0500

    adds home page details

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,18 @@
-
+import { Link } from 'react-router-dom'
 function HomePage(props) {
     return (
         <main className="Page">
             <div className="HomePage">
 
-            <img src="src/images/poster.jpg" alt="" width="500" height="600"/>
+            <div className="Words">
+
+                <h1>Welcome to #Trending!</h1>
+                <p>A site where you will be able to see the Movies or Shows that are trending Today</p>
+                <p>If you are new to the site <Link to='/signup'>Signup</Link>!</p>
+                <p>Otherwise <Link to="login">Login</Link></p>
+                <p>Have fun!</p>
             
+            </div>
 
             </div>
         </main>
